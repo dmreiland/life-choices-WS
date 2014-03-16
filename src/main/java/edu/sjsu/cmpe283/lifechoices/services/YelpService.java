37dff178478c6b4447481be27a902308ae168a88
Sai <saikarra@gmail.com>
2014-03-16 16:58:57 -0700

    Added Yelp at /v1/food/

---
+++ src/main/java/edu/sjsu/cmpe283/lifechoices/services/YelpService.java
@@ -0,0 +1,55 @@
+package edu.sjsu.cmpe283.lifechoices.services;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+import org.springframework.stereotype.Service;
+
+import edu.sjsu.cmpe283.lifechoices.config.Props;
+
+/**
+ * User: srkarra
+ * Date: 3/16/14 - 10:31 AM
+ */
+@Service
+public class YelpService {
+    
+    private static Log logger = LogFactory.getLog(GooglePlacesService.class);
+    
+    private OAuthService service;
+    private Token accessToken;
+    
+    /**
+     * Returns 
+     * @param searchTerm search term
+     * @param latitude Latitude
+     * @param longitude Location
+     * @return Yelp JSON response
+     */
+    public String getFoodPlaces(String searchTerm, double latitude, double longitude) {
+        String response = "";
+        // Configure OAuth
+        if(service == null || accessToken == null) {
+            this.service = new ServiceBuilder().provider(YelpServiceProvider.class).apiKey(Props.YELP_CONSUMER_KEY).apiSecret(Props.YELP_COSUMER_SECRET).build();
+            this.accessToken = new Token(Props.YELP_TOKEN_KEY, Props.YELP_TOKEN_SECRET);
+        }
+        
+        
+        // Create and send request
+        OAuthRequest request = new OAuthRequest(Verb.GET, Props.YELP_API_URL);
+        request.addQuerystringParameter("term", searchTerm == null ? "restaurants" : searchTerm);
+        request.addQuerystringParameter("ll", latitude + "," + longitude);
+        this.service.signRequest(this.accessToken, request);
+        try {
+            response =  request.send().getBody();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+}
---
+++ src/main/java/edu/sjsu/cmpe283/lifechoices/webservices/FoodWS.java
@@ -0,0 +1,39 @@
+package edu.sjsu.cmpe283.lifechoices.webservices;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.sjsu.cmpe283.lifechoices.services.YelpService;
+
+/**
+ * User: srkarra
+ * Date: 3/16/14 - 4:00 PM
+ */
+@RestController
+@RequestMapping("/v1/food")
+public class FoodWS {
+    private static Log logger = LogFactory.getLog(FoodWS.class);
+
+    @Autowired
+    YelpService yelpService;
+
+    @RequestMapping(value = "/{latitude}/{longitude}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity userGeoHistory(
+            @PathVariable("latitude") double latitude,
+            @PathVariable("longitude") double longitude,
+            @RequestParam(value = "keyword", required = false) String searchTerm
+
+    ) {
+        logger.info("latitude=[" + latitude + "], longitude=[" + longitude + "], type=[" + searchTerm + "]");
+        String response = yelpService.getFoodPlaces(searchTerm, latitude, longitude);
+        return new ResponseEntity<String>(response, HttpStatus.OK);
+    }
+}
