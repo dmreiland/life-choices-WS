878bc0b607b432b7744ce5eeb312841cbcfa72c3
maksimu <maksimu@gmail.com>
2014-03-15 22:41:05 -0700

    - updated some docs - disabled unit test for mvn

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### Instruction to start web service:
 
+Java
+====
+This project will only compile with Java 7 (1.7) [Download Here](http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html)
+[This](https://github.com/spring-projects/spring-boot/pull/497) is a REASON/Problem why it is not possible to execute this project with Java 1.6
+
 #### Maven
 To build and run a project artifact, you need to navigate to the web service root folder (`cd {$project location}/web-service/`) and perform following tasks using Maven:
 
@@ -16,6 +21,13 @@ Configure IDE to execute main method in LifeChoicesApp class
 
 
 #### Test
-Then navigate to This is [http://localhost:8080/v1/geo/](http://localhost:8080/v1/geo/)
+For testing navigation to this user [http://localhost:8080/v1/geo/](http://localhost:8080/v1/geo/)
 
-Default login is user:password
+### Documentation
+- [Web services](https://github.com/cmpe273-indexzero/life-choices-WS/tree/master/src/main/java/edu/sjsu/cmpe283/lifechoices/webservices)
+- [Entities](https://github.com/cmpe273-indexzero/life-choices-WS/tree/master/src/main/java/edu/sjsu/cmpe283/lifechoices/entities)
+- [Configs](https://github.com/cmpe273-indexzero/life-choices-WS/tree/master/src/main/java/edu/sjsu/cmpe283/lifechoices/config)
+
+
+
+
--- src/main/java/edu/sjsu/cmpe283/lifechoices/webservices/GeoWS.java
+++ src/main/java/edu/sjsu/cmpe283/lifechoices/webservices/GeoWS.java
@@ -29,16 +29,6 @@ public class GeoWS {
     @Autowired
     UserGeoHistoryService userGeoHistoryService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<HashMap<String, Object>> availableMethods() {
-
-        HashMap<String, Object> hashMap = new HashMap<String, Object>();
-        hashMap.put("message", "hello from the server");
-        hashMap.put("status", "ok");
-
-
-        return new ResponseEntity<HashMap<String, Object>>(hashMap, HttpStatus.OK);
-    }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public ResponseEntity postNewGeoHistory(@RequestBody(required = true) @Valid GeoHistoryDTO geoHistoryDTO) {
@@ -93,4 +83,63 @@ public class GeoWS {
     }
 
 
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/plain")
+    public ResponseEntity availableMethods() {
+
+        String txt =
+                "New geo location\n" +
+                "----------------------\n" +
+                "\n" +
+                "URI: **/v1/geo/**\n" +
+                "Method: **POST**\n" +
+                "Consume type: **application/json**\n" +
+                "Request Payload:\n" +
+                "\n" +
+                "{\n" +
+                "    \"userName\":\"maksim\",\n" +
+                "    \"timestamp\":1394932551,\n" +
+                "    \"latitude\" : 37.415365 ,\n" +
+                "    \"longitude\" : -122.089803\n" +
+                " }\n" +
+                "Response type: **application/json**\n" +
+                "Response Payload Sample:\n" +
+                "\n" +
+                "{\n" +
+                "    \"id\":\"5324fb6803647201bbbfe4da\",\n" +
+                "    \"timestamp\":1.39493261E9,\n" +
+                "    \"latitude\":37.415363,\n" +
+                "    \"longitude\":-122.089806,\n" +
+                "    \"userName\":\"maksim\"\n" +
+                "}\n" +
+                "Response Status: **201 Created**\n" +
+                "\n" +
+                "User Location History\n" +
+                "---------------------\n" +
+                "URI: **/v1/geo/history/{username}?starttime=123&endtime=234** *(starttime & endtime are optional. Default value will be a time BETWEEN now and 1 hr ago)*\n" +
+                "Method: **GET**\n" +
+                "Consume type: N/A\n" +
+                "Request Payload: N/A\n" +
+                "Response type: **application/json**\n" +
+                "Response Payload Sample:\n" +
+                "\n" +
+                "Example URL: **/v1/geo/history/maksim?starttime=1394939000&endtime=1394939999**\n" +
+                "\n" +
+                "{\n" +
+                "    \"endtime\":1394939000,\n" +
+                "    \"username\":\"maksim\",\n" +
+                "    \"starttime\":1394939999,\n" +
+                "    \"history\":[\n" +
+                "                {\n" +
+                "                    \"id\":\"5325267e0364bc3dfe32da27\",\n" +
+                "                    \"timestamp\":1394939032,\n" +
+                "                    \"position\":[37.41536331176758,-122.08980560302734],\n" +
+                "                    \"userName\":\"maksim\"\n" +
+                "                }\n" +
+                "              ]\n" +
+                "}\n" +
+                "\n";
+
+
+        return new ResponseEntity<String>(txt, HttpStatus.OK);
+    }
 }
