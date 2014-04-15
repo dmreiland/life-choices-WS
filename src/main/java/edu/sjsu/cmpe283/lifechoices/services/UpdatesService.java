41a61649686927564cb5a77af5a786f11db9374a
Sai <saikarra@gmail.com>
2014-04-14 21:54:12 -0700

    Added Weather API as part of the updates service

--- src/main/java/edu/sjsu/cmpe283/lifechoices/services/UpdatesService.java
+++ src/main/java/edu/sjsu/cmpe283/lifechoices/services/UpdatesService.java
@@ -1,30 +1,24 @@
 package edu.sjsu.cmpe283.lifechoices.services;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.sjsu.cmpe283.lifechoices.utils.HTTPUtils;
 import edu.sjsu.cmpe283.lifechoices.webservices.dto.UpdatesDTO;
 import edu.sjsu.cmpe283.lifechoices.webservices.dto.UpdatesDTO.Destinations;
 import edu.sjsu.cmpe283.lifechoices.webservices.dto.domain.GoogleMapsDirections;
+import edu.sjsu.cmpe283.lifechoices.webservices.dto.domain.Weather;
 
 @Service
 public class UpdatesService {
@@ -33,6 +27,9 @@ public class UpdatesService {
     
     @Value("${google.places.apikey}")
     String GOOGLE_PLACES_API_KEY;
+    
+    @Value("${weather.apikey}")
+    String WUNDERGROUND_API;
     
     
     /**
@@ -69,6 +66,7 @@ public class UpdatesService {
         UpdatesDTO updates = new UpdatesDTO();
         updates.setLatitude(originLatitude);
         updates.setLongitude(originLongitude);
+        updates.setWeather(getWeatherData(originLatitude, originLongitude));
         
         for (SimpleEntry<Double, Double> seed : getRandomSeedDataForUpdates()) {
             // NOTE: I'm "hacking" here, should move this to an object instead of the SimpleEntry Object
@@ -84,6 +82,8 @@ public class UpdatesService {
             // Get Static Map
             destination.setGoogleMapsStaticLink(getGoogleStaticMapsURL(width, height, originLatitude, originLongitude, seed.getKey(), seed.getValue(), destination.getRawDirections().getRoutes().get(0).getPolyLine().getPoints()));
             
+            // Get Weather Info
+            destination.setWeather(getWeatherData(seed.getKey(), seed.getValue()));
             
             // Store Destination
             updates.addDesination(destination);
@@ -120,30 +120,7 @@ public class UpdatesService {
     public GoogleMapsDirections getGoogleDirections(double originLatitude, double originLongitude, double destinationLatitude, double destinationLongitude) throws IOException {
         // Construct URL
         String directionURL = String.format("https://maps.googleapis.com/maps/api/directions/json?sensor=false&key=%s&origin=%s,%s&destination=%s,%s", GOOGLE_PLACES_API_KEY, originLatitude, originLongitude, destinationLatitude, destinationLongitude);
-        
-        // Send GET Request
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet get = new HttpGet(directionURL);
-        CloseableHttpResponse response = httpclient.execute(get);
-        
-        // Get String
-        String data = "";
-        try {
-            HttpEntity entity = response.getEntity();
-            
-            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                data += line;
-            } 
-            
-            data = data.replaceAll("\\s+", " ");
-            EntityUtils.consumeQuietly(entity);
-        }
-        finally{
-            response.close();
-        }
-        
+        String data = HTTPUtils.HTTPGet(directionURL);
         
         // Construct POJO
         GoogleMapsDirections directions = new ObjectMapper()
@@ -174,6 +151,28 @@ public class UpdatesService {
         String url = String.format("http://maps.google.com/maps/api/staticmap?key=%s&size=%dx%d&maptype=roadmap&sensor=false&markers=%s,%s|%s,%s&path=weight:3|color:blue|enc:%s"
                 , GOOGLE_PLACES_API_KEY, width, height, originLat, originLong, destinationLat, destinationLong, points);
         return url;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public Weather getWeatherData(Double latitude, Double longitude) throws IOException {
+        String weatherURL = String.format("http://api.wunderground.com/api/%s/forecast/q/%s,%s.json", WUNDERGROUND_API, latitude, longitude);
+        String data = HTTPUtils.HTTPGet(weatherURL);
+        
+        Weather weather =  new ObjectMapper()
+//              .setVisibility(PropertyAccessor.FIELD, Visibility.ANY)
+              .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+//              .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+              .enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+              .readValue(data, Weather.class);
+        return weather;
     }
     
 }
