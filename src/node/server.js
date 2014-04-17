454e5b886e850e530e0f1e7e6f9a67c0ad18a5d8
maksimu <maksimu@gmail.com>
2014-04-17 09:46:27 -0700

    - added trigger to pull and restart the server

--- src/node/server.js
+++ src/node/server.js
@@ -8,3 +8,46 @@ var connect = require('connect');
 console.log("Starting ...");
 connect.createServer(connect.static(__dirname)).listen(8888);
 console.log("Started static web site on port: " + 8888);
+
+
+
+var sys = require('sys');
+var exec = require('child_process').exec;
+var express = require('express');
+var app = express();
+
+app.get('/pull-restart', function (req, res) {
+
+    exec("/home/ubuntu/life-choices-WS/pull-restart.sh",
+
+        function (error, stdout, stderr) {
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+
+            if (error !== null) {
+                console.log('exec error: ' + error);
+                var json = {
+                    "status":"error",
+                    "message": "exec error:" + error,
+                    "time": new Date()
+                };
+
+                res.send(json);
+            } else {
+
+                var json = {
+                    "status":"success",
+                    "message": "good job",
+                    "time": new Date()
+                };
+
+                res.send(json);
+            }
+        });
+
+
+});
+
+console.log("Started web services on port: " + 9999);
+app.listen(process.env.PORT || 9999);
+
