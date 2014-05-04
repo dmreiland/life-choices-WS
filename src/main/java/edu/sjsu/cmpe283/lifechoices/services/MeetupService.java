70ea66b7fa13bb6ea22ebddc4f37d2f2cacdf0b8
Sai <saikarra@gmail.com>
2014-05-03 23:00:12 -0700

    Added Simple Meetup API

---
+++ src/main/java/edu/sjsu/cmpe283/lifechoices/services/MeetupService.java
@@ -0,0 +1,49 @@
+package edu.sjsu.cmpe283.lifechoices.services;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import edu.sjsu.cmpe283.lifechoices.utils.HTTPUtils;
+
+@Service
+public class MeetupService {
+
+    private static Log logger = LogFactory.getLog(MeetupService.class);
+
+    @Value("${meetup.api.key}")
+    String MEETUP_API_KEY;
+    
+    @Value("${meetup.api.url}")
+    String MEETUP_API_URL;
+    
+    private static final int MEETUP_RADIUS = 100; 
+    
+    
+    public String getEvents(Double latitude, Double longitude, String category, Integer page, Integer radius) throws IOException {
+        if(radius != null &&    radius > MEETUP_RADIUS) {
+            radius = MEETUP_RADIUS;
+        }
+        
+        String url =  String.format("%s/open_events?format=json&lat=%s&lon=%s&page=%s&desc=true%s%s&key=%s",MEETUP_API_URL,latitude,longitude,page,
+                category != null ? "&category=" + category : "", radius != null ? "&radius=" + radius : "", MEETUP_API_KEY);
+        
+        logger.info(String.format("Fetching events with lat: '%s', long: '%s', category: '%s', page: '%s', radius: '%s'. URL: %s", latitude, longitude, category, page, radius, url));
+        return HTTPUtils.HTTPGet(url);
+    }
+    
+    
+    
+    
+    public String getCategories() throws IOException {
+        String url = String.format("%s/categories?format=json&key=%s", MEETUP_API_URL, MEETUP_API_KEY);
+        logger.info("Fetching Event Categories: " + url);
+        return HTTPUtils.HTTPGet(url);
+    }
+    
+    
+}
+
